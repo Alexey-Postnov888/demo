e0daf609044be80a25c22d277fc9cc12e1b1a93e
Alexey Postnov <adpostnov@yandex.ru>
2025-01-22 20:52:55 +0500

    Main page is returned

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class PostsViewController {
     @ResponseBody
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list() {
+        return "Здесь будет главная страница";
+    }
+
+    @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
