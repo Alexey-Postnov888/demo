eb2c091630d9597dff3daecb113f87e69d0c72dd
Alexey Postnov <adpostnov@yandex.ru>
2025-01-23 19:52:36 +0500

    Bootstrap and post creation date are added

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
-        Post postFirst = new Post("Post 1");
-        Post postSecond = new Post("Post 2");
-        Post postThird = new Post("Post 3");
+        Post postFirst = new Post("Post 1", new Date());
+        Post postSecond = new Post("Post 2", new Date());
+        Post postThird = new Post("Post 3", new Date());
 
         return new ArrayList<>(Arrays.asList(postFirst, postSecond, postThird));
     }
